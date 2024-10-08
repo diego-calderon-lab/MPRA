9298a9842bb48c57d603b4b656484b319fb31892
Angela Gao <38025040+lastarangela@users.noreply.github.com>
2024-10-08 12:13:35 -0700

    Add data input code

--- DataNormalization.py
+++ DataNormalization.py
@@ -1,9 +1,16 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
+Created on Sat Oct  5 2024
 
-This is a temporary script file.
+@author: angela_gao
 """
 
-print('Hello world!')
+import pandas as pd
 
+##################################################################################################
+####################################### DATA INPUT ###############################################
+##################################################################################################
+
+HepG2_SUD1_minP_mRNA_Rep1 = pd.read_csv('/Users/angela_gao/Dropbox/Angela Gao - UCSF Tetrad/Calderon Lab Rotation/MPRA_Project/MPRA_dataset/GSE71279_RAW/GSM1831757_HepG2_ScaleUpDesign1_minP_mRNA_Rep1.counts.txt', sep='\t')
+HepG2_SUD1_minP_mRNA_Rep2 = pd.read_csv('/Users/angela_gao/Dropbox/Angela Gao - UCSF Tetrad/Calderon Lab Rotation/MPRA_Project/MPRA_dataset/GSE71279_RAW/GSM1831758_HepG2_ScaleUpDesign1_minP_mRNA_Rep2.counts.txt', sep='\t')
+SUD1_minP_DNA = pd.read_csv('/Users/angela_gao/Dropbox/Angela Gao - UCSF Tetrad/Calderon Lab Rotation/MPRA_Project/MPRA_dataset/GSE71279_RAW/GSM1831773_ScaleUpDesign1_minP_Plasmid.counts.txt', sep='\t')
